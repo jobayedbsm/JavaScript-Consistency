32f1c81bf277c4b0ee8cdf2f89b7bb826341356b
jobayed24 <jkhouse117@gmail.com>
2026-02-25 12:13:21 +0600

    ternary operator used in boolean value

--- ternary.js
+++ ternary.js
@@ -1,27 +1,4 @@
-const UserRole = {
-  ADMIN: 'admin',
-  EDITOR: 'editor',
-  VIEWER: 'viewer'
-};
-
-
-function getDashboardAccess(userRole) {
-
-    switch (userRole) {
-        case UserRole.ADMIN:
-            return 'admin/dashboard';
-        case UserRole.EDITOR:
-            return 'editor/dashboard';
-        case UserRole.VIEWER:
-            return 'viewer/dashboard';
-        default:
-             throw new Error('Invalid user role');
-    }
-}
-
-try {
-    let user=getDashboardAccess(UserRole.EDITOR); // Output: 'Full access to dashboard'
-console.log(user);
-}catch (error) {    
-    console.error(error.message) // Output: 'Invalid user role'
-}
+let x=10;
+let y=5;
+let result=(x>10 && y<6) ? "x is big" : "y is less";
+console.log(result);
