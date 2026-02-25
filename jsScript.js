e284157cfa019faa9de75baebb4cf4281ac1796b
jobayed24 <jkhouse117@gmail.com>
2026-02-25 12:17:18 +0600

    the logical not retrn true for false and false for true

---
+++ jsScript.js
@@ -0,0 +1,4 @@
+let x=10;
+let y=5;
+let result=!(x>10 || y>3) ? "True" : "False";
+console.log(result);
--- ternary.js
+++
@@ -1,4 +0,0 @@
-let x=10;
-let y=5;
-let result=(x>10 && y<6) ? "x is big" : "y is less";
-console.log(result);
